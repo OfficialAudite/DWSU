cc0879c9dcc6fbd03b207e7e3893d3c0ec9e8ef9
OfficialAudite <dovydas.bumblauskas@aol.com>
2024-09-13 01:44:44 +0200

    Add versioning

---
+++ app/util/version/version.go
@@ -0,0 +1,44 @@
+package version
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	_ "embed"
+)
+
+const (
+	GH_API_URL = "https://api.github.com/repos/OfficialAudite/DWSU"
+)
+
+//go:embed version.txt
+var CurrentVersion string
+var LatestVersion = GetLatestVersion()
+
+func IsLatestVersion() bool {
+	return CurrentVersion >= LatestVersion
+}
+
+func GetLatestVersion() string {
+	res, err := http.Get(GH_API_URL + "/releases/latest")
+	if err != nil {
+		return ""
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return ""
+	}
+
+	var release struct {
+		TagName string `json:"tag_name"`
+	}
+	err = json.Unmarshal(body, &release)
+	if err != nil {
+		return ""
+	}
+
+	return release.TagName
+}
